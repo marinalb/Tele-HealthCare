a4f7766f603d9f93a7106dc9d9853d3b50abcfee
11104974 <11104974@W1975390.portoalegre.pucrsnet.br>
2016-10-27 22:39:56 -0200

    #5

---
+++ Tele-HealthCare/test/Business/AllergyList/AllergyTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.AllergyList;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 11104974
+ */
+public class AllergyTest {
+    
+    public AllergyTest() {
+    }
+    
+}
